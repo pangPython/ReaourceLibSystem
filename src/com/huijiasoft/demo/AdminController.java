0600732e0e19ffe53783bebe93a3f8c568e602f1
pangPython <1253973173@qq.com>
2016-09-22 19:40:31 +0800

    first commit

---
+++ src/com/huijiasoft/demo/AdminController.java
@@ -0,0 +1,9 @@
+package com.huijiasoft.demo;
+
+import com.jfinal.core.Controller;
+
+public class AdminController extends Controller {
+	public void index(){
+		render("index.html");
+	}
+}
---
+++ src/com/huijiasoft/demo/HelloContrlller.java
@@ -0,0 +1,9 @@
+package com.huijiasoft.demo;
+
+import com.jfinal.core.Controller;
+
+public class HelloContrlller extends Controller {
+	public void index(){
+		render("index.html");
+	}
+}
