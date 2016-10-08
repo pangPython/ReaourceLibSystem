514001d37deea8d0cf9bf4a2aec04ea0f5dcb15c
pangPython <1253973173@qq.com>
2016-10-08 20:00:45 +0800

    修改用户session存储

--- src/com/huijiasoft/controller/UserController.java
+++ src/com/huijiasoft/controller/UserController.java
@@ -29,20 +29,7 @@ public class UserController extends Controller {
 			setAttr("user", user);
 			
 			if(user.getAge()==null || user.getArtType()==null || user.getCard()==null){
-				List<Area> area = Area.dao.getAllArea();
-				List<Mz> minzu = Mz.dao.getAllMz();
-				List<Zzmm> zzmmList = Zzmm.dao.getAllZzmm();
-				List<Edu> eduList = Edu.dao.getAllEdu();
-				List<Degree> degreeList = Degree.dao.getAllDegree();
-				List<DeclareType> decList = DeclareType.dao.getAllDecType();
-				
-				setAttr("area", area);
-				setAttr("minzuList",minzu);
-				setAttr("zzmmList", zzmmList);
-				setAttr("eduList", eduList);
-				setAttr("degreeList",degreeList);
-				setAttr("decList", decList);
-				render("/adduserinfo.html");
+				redirect("/adduserinfopage");
 				
 			}else{
 				
@@ -86,6 +73,27 @@ User user = (User) getSession().getAttribute(getCookie("cuser"));
 		getModel(User.class).set("id", user.getId()).update();
 		setAttr("user", user);
 		render("/sbmtsucc.html");
+	}
+	
+	@ActionKey("/adduserinfopage")
+	public void adduserinfopage(){
+		User user = (User) getSession().getAttribute(getCookie("cuser"));
+		
+		setAttr("user", user);
+		List<Area> area = Area.dao.getAllArea();
+		List<Mz> minzu = Mz.dao.getAllMz();
+		List<Zzmm> zzmmList = Zzmm.dao.getAllZzmm();
+		List<Edu> eduList = Edu.dao.getAllEdu();
+		List<Degree> degreeList = Degree.dao.getAllDegree();
+		List<DeclareType> decList = DeclareType.dao.getAllDecType();
+		
+		setAttr("area", area);
+		setAttr("minzuList",minzu);
+		setAttr("zzmmList", zzmmList);
+		setAttr("eduList", eduList);
+		setAttr("degreeList",degreeList);
+		setAttr("decList", decList);
+		render("/adduserinfo.html");
 	}
 
 }
