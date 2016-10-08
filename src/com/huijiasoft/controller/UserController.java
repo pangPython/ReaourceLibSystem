62eaee3435ec1dbac4f6c292ecdac1dc7dd48e79
pangPython <1253973173@qq.com>
2016-10-08 17:00:36 +0800

    完成注册页面的下拉框

--- src/com/huijiasoft/controller/UserController.java
+++ src/com/huijiasoft/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.huijiasoft.controller;
 
+import java.util.List;
+
 import com.huijiasoft.interceptor.UserAuthInterceptor;
+import com.huijiasoft.model.Area;
+import com.huijiasoft.model.DeclareType;
+import com.huijiasoft.model.Degree;
+import com.huijiasoft.model.Edu;
+import com.huijiasoft.model.Mz;
 import com.huijiasoft.model.User;
+import com.huijiasoft.model.Zzmm;
 import com.jfinal.aop.Before;
+import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
 
 /**
@@ -19,7 +28,20 @@ public class UserController extends Controller {
 		
 			setAttr("user", user);
 			
-			if(user.getAge()==null){
+			if(user.getAge()==null || user.getArtType()==null || user.getCard()==null){
+				List<Area> area = Area.dao.getAllArea();
+				List<Mz> minzu = Mz.dao.getAllMz();
+				List<Zzmm> zzmmList = Zzmm.dao.getAllZzmm();
+				List<Edu> eduList = Edu.dao.getAllEdu();
+				List<Degree> degreeList = Degree.dao.getAllDegree();
+				List<DeclareType> decList = DeclareType.dao.getAllDecType();
+				
+				setAttr("area", area);
+				setAttr("minzuList",minzu);
+				setAttr("zzmmList", zzmmList);
+				setAttr("eduList", eduList);
+				setAttr("degreeList",degreeList);
+				setAttr("decList", decList);
 				render("/adduserinfo.html");
 				
 			}else{
@@ -54,9 +76,16 @@ User user = (User) getSession().getAttribute(getCookie("cuser"));
 	
 	public void edit(){
 		User user = (User) getSession().getAttribute(getCookie("cuser"));
-		
 		setAttr("user", user);
 		render("/editinfo.html");
+	}
+	
+	@ActionKey("/adduserinfo")
+	public void adduserinfo(){
+		User user = (User) getSession().getAttribute(getCookie("cuser"));
+		getModel(User.class).set("id", user.getId()).update();
+		setAttr("user", user);
+		render("/sbmtsucc.html");
 	}
 
 }
