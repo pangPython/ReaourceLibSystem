e1b20b712625fd59f81b486d36f08adc9b1c4e3b
pangPython <1253973173@qq.com>
2016-10-27 00:13:42 +0800

    上传音频视频资料、前台JS校验

--- src/com/huijiasoft/controller/UserController.java
+++ src/com/huijiasoft/controller/UserController.java
@@ -30,271 +30,285 @@ import com.jfinal.upload.UploadFile;
 import com.oreilly.servlet.MultipartRequest;
 
 /**
- * @author pangPython
- *		用户控制器
+ * @author pangPython 用户控制器
  */
 @Before(UserAuthInterceptor.class)
 public class UserController extends Controller {
-	
-	 private final int MAXSize = 50 * 1024 * 1024; // 5M
-	
+
+	private final int MAXSize = 50 * 1024 * 1024; // 5M
+
 	public void index() {
-		
+
 		User user = getSessionAttr(getCookie("cuser"));
-		setAttr("user",user);
-		
+		setAttr("user", user);
+
 		render("index.html");
 	}
-	
-	//中心
-	public void welcome(){
+
+	// 中心
+	public void welcome() {
 		User user = getSessionAttr(getCookie("cuser"));
 		setAttr("user", user);
 		render("welcome.html");
 	}
-	
-	
-	//用户信息查看
+
+	// 用户信息查看
 	public void info() {
 		User user = (User) getSessionAttr(getCookie("cuser"));
-		
+
 		setAttr("user", user);
 		render("info.html");
 	}
-	//已经登录用户的退出操作
-	public void logout(){
+
+	// 已经登录用户的退出操作
+	public void logout() {
 		removeSessionAttr(getCookie("cuser"));
 		removeCookie("cuser");
 		redirect("/");
 	}
-	
-	//个人音频视频资料
-	public void mediainfo(){
+
+	// 个人音频视频资料
+	public void mediainfo() {
 		User user = (User) getSession().getAttribute(getCookie("cuser"));
 		setAttr("user", user);
 		setAttr("mediaList", Uploads.dao.getAllMediaById(user.getId().toString()));
 		render("/mediainfo.html");
 	}
-	
-	
-	public void edit(){
+
+	public void edit() {
 		User user = (User) getSession().getAttribute(getCookie("cuser"));
-		
+
 		setAttr("user", user);
 		setAttr("areaList", Area.dao.getAllArea());
 		setAttr("nationList", Mz.dao.getAllMz());
 		setAttr("zzmmList", Zzmm.dao.getAllZzmm());
 		setAttr("eduList", Edu.dao.getAllEdu());
 		setAttr("degreeList", Degree.dao.getAllDegree());
-		
+
 		render("edit.html");
 	}
-	
-	//修改个人信息
-	public void updateinfo(){
+
+	// 修改个人信息
+	public void updateinfo() {
 		User user = (User) getSession().getAttribute(getCookie("cuser"));
-		getModel(User.class).set("id", user.getId()).set("status",0).update();
+		getModel(User.class).set("id", user.getId()).set("status", 0).update();
 		user = User.usermodel.findById(user.getId());
-		setSessionAttr(getCookie("cuser"),user);
+		setSessionAttr(getCookie("cuser"), user);
 		renderText("修改成功！");
 	}
-	
-	
-	public void media_upload(){
-		
+
+	public void media_upload() {
+
 	}
-	
-	
-	
+
 	// 用户上传媒体文件
-	public void upload() {}
-	
-	//上传一寸照片
-	public void upload_person_photo(){
-		User user =  getSessionAttr(getCookie("cuser"));
+	public void upload() {
+	}
+
+	// 上传一寸照片
+	public void upload_person_photo() {
+		User user = getSessionAttr(getCookie("cuser"));
 		user.setPhotoPath(getFile().getFileName());
 		user.update();
 		renderText("上传成功！");
-		
+
 	}
-	
-	public void media_pic(){
+
+	public void media_pic() {
 		User user = getSessionAttr(getCookie("cuser"));
-		//int user_id = user.getId();
-		String path = PathKit.getWebRootPath()+"\\upload\\photo\\"+user.getMediaPath()+"\\";
+		// int user_id = user.getId();
+		String path = PathKit.getWebRootPath() + "\\upload\\photo\\" + user.getMediaPath() + "\\";
 		List<String> list = PathUtils.getAllFilePath(path);
-		
+
 		setAttr("picList", list);
 		setAttr("user", user);
 		render("media_pic.html");
 	}
-	//测试
-	public void ttt(){
+
+	// 测试
+	public void ttt() {
 		User user = getSessionAttr(getCookie("cuser"));
-		String path = PathKit.getWebRootPath()+"\\upload\\photo\\"+user.getMediaPath()+"\\";
+		String path = PathKit.getWebRootPath() + "\\upload\\photo\\" + user.getMediaPath() + "\\";
 		List<String> list = PathUtils.getAllFilePath(path);
 		String str = "";
-		
+
 		for (int i = 0; i < list.size(); i++) {
 			str += list.get(i);
 		}
-//		Iterator<String> it = list.iterator();
-//		
-//		String str = "";
-//		while(it.hasNext()){
-//			System.out.println(it.next());
-//		}
-		
+
 		renderText(str);
 	}
-	
-	//显示页面
-	public void media_pic_upload(){}
-	
-	//多图片上传
+
+	// 显示页面
+	public void media_pic_upload() {
+	}
+
+	// 多图片上传
 	@ActionKey("/user/upload_pic")
-	public void upload_pic() throws IOException{
+	public void upload_pic() throws IOException {
 		User user = getSessionAttr(getCookie("cuser"));
 
 		String picture_upload_path = user.getMediaPath();
-		
-		//处理上传路径
-		if(!PathUtils.createNotExist(PathKit.getWebRootPath()+"\\upload\\photo\\"+picture_upload_path)){
+
+		// 处理上传路径
+		if (!PathUtils.createNotExist(PathKit.getWebRootPath() + "\\upload\\photo\\" + picture_upload_path)) {
 			renderText("上传路径出错!");
 			return;
 		}
-		
-		//处理多文件上传
-		getFiles("\\photo\\"+picture_upload_path, MAXSize, "utf-8");
-		
-		renderText("上传成功！");
-	
+
+		// 处理多文件上传
+		getFiles("\\photo\\" + picture_upload_path, MAXSize, "utf-8");
+
+		renderText("图片资料上传成功！");
+
 	}
-	
-	//显示用户审核是否通过的管理员消息
-	public void msg(){
+
+	// 显示用户审核是否通过的管理员消息
+	public void msg() {
 		User user = getSessionAttr(getCookie("cuser"));
-		if(user.getStatus()==1){
+		if (user.getStatus() == 1) {
 			render("msg-success.html");
 			return;
-		}else{
+		} else {
 			render("msg-failed.html");
 			return;
 		}
 	}
-	
-	
-	public void media_audio(){
-		
-	}
-	
-public void media_audio_upload(){
-		
-	}
-	
-	public void media_video(){
-		
-	}
-	
-	
-public void media_video_upload(){
-		
+
+	public void media_audio() {
+
 	}
 
+	public void media_audio_upload() {
+		
+	}
 	
+	public void upload_audio() {
+		User user = getSessionAttr(getCookie("cuser"));
 
+		String picture_upload_path = user.getMediaPath();
+
+		// 处理上传路径
+		if (!PathUtils.createNotExist(PathKit.getWebRootPath() + "\\upload\\audio\\" + picture_upload_path)) {
+			renderText("上传路径出错!");
+			return;
+		}
+
+		// 处理多文件上传
+		getFiles("\\audio\\" + picture_upload_path, MAXSize, "utf-8");
+
+		renderText("音频资料上传成功！");
+
+	}
+
+	public void media_video() {
+
+	}
+
+	public void media_video_upload() {
+		
+	}
 	
-	
-	public void application_std(){}
-	public void application_my(){}
-	
-	//下载标准报名表
+	public void upload_video(){
+		User user = getSessionAttr(getCookie("cuser"));
+
+		String picture_upload_path = user.getMediaPath();
+
+		// 处理上传路径
+		if (!PathUtils.createNotExist(PathKit.getWebRootPath() + "\\upload\\video\\" + picture_upload_path)) {
+			renderText("上传路径出错!");
+			return;
+		}
+
+		// 处理多文件上传
+		getFiles("\\video\\" + picture_upload_path, MAXSize, "utf-8");
+
+		renderText("视频资料上传成功！");
+	}
+
+	public void application_std() {
+	}
+
+	public void application_my() {
+	}
+
+	// 下载标准报名表
 	@ActionKey("/user/download_std")
-	public void download_std(){
+	public void download_std() {
 		renderFile(new File("WebRoot\\download\\application\\东营市文化艺术人才信息登记表.doc"));
 	}
-	
-	public void download_my(){
+
+	public void download_my() {
 		User user = getSessionAttr(getCookie("cuser"));
-		
-		if(DBUtils.RecordAttrHasNull(user)){
+
+		if (DBUtils.RecordAttrHasNull(user)) {
 			renderText("你的报名信息不完整,请先完善信息！");
 			return;
 		}
-		
-		String file_name = RenderDocxTemplate.creatWord(user);
-		renderFile(new File(file_name));	
-	}
-	
-	
-	
 
-	
-	//显示修改密码页面
-	public void change_pwd(){
+		String file_name = RenderDocxTemplate.creatWord(user);
+		renderFile(new File(file_name));
+	}
+
+	// 显示修改密码页面
+	public void change_pwd() {
 		User user = (User) ControllerUtils.getMFromSbyIdinC(this, "cuser");
 		setAttr("user", user);
 		render("change_pwd.html");
 	}
-	
-	//修改密码
-	//TODO
-	public void updatepwd(){
-		
+
+	// 修改密码
+	// TODO
+	public void updatepwd() {
+
 		User user = (User) ControllerUtils.getMFromSbyIdinC(this, "cuser");
-		//验证码
+		// 验证码
 		boolean yzm = this.validateCaptcha("yzm");
-		if(!yzm){
-			
+		if (!yzm) {
+
 			setAttr("user", user);
 			setAttr("yzmErrMsg", "请正确输入验证码！");
 			render("change_pwd.html");
 			return;
 		}
-		
+
 		String reg_time = user.getRegDate();
-		
-		//验证原密码
-		String old_pwd = MD5.GetMD5Code(getPara("oldpwd")+reg_time);
-		if(!user.getPwd().equals(old_pwd)){
+
+		// 验证原密码
+		String old_pwd = MD5.GetMD5Code(getPara("oldpwd") + reg_time);
+		if (!user.getPwd().equals(old_pwd)) {
 			setAttr("user", user);
 			setAttr("oldpwdErrMsg", "请正确输入原密码！");
 			render("change_pwd.html");
 			return;
 		}
-		
-		//比对确认密码		
+
+		// 比对确认密码
 		String new_pwd = getPara("newpwd");
 		String confirm_new_pwd = getPara("confirmnewpwd");
-		
-		
-		if(!new_pwd.equals(confirm_new_pwd)){
+
+		if (!new_pwd.equals(confirm_new_pwd)) {
 			setAttr("user", user);
 			setAttr("confirmpwdErrMsg", "请正确输入新密码！");
 			render("change_pwd.html");
 			return;
 		}
-		
-		//新旧密码是否相同
-		new_pwd = MD5.GetMD5Code(new_pwd+reg_time);
-		
-		if(new_pwd.equals(old_pwd)){
+
+		// 新旧密码是否相同
+		new_pwd = MD5.GetMD5Code(new_pwd + reg_time);
+
+		if (new_pwd.equals(old_pwd)) {
 			setAttr("user", user);
 			setAttr("newpwdErrMsg", "旧密码与新密码相同！");
 			render("change_pwd.html");
 			return;
 		}
-		
-		//修改密码
+
+		// 修改密码
 		user.setPwd(new_pwd);
 		user.update();
 		renderText("密码更改成功！");
-		
+
 	}
-	
-	
-	
-	
-	
 
 }
